e7f5a940e0e9a6edcf88163006c6b4b0f61e71be
Likenesio <valentinubb@gmail.com>
2024-06-11 10:02:22 -0400

    Se modifica el usuario

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 'use strict';
 require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
+const app = express(); 
 
 app.use(cors());
 app.options('*', cors());
