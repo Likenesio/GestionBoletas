63f3944cddc18f5db6e7f4d56f18855a0c4697d8
Likenesio <valentinubb@gmail.com>
2024-06-13 14:52:51 -0400

    Se modifica dashboard

--- frontend/src/components/dashboard/Dashboard.vue
+++ frontend/src/components/dashboard/Dashboard.vue
@@ -1,78 +1,137 @@
 <template>
   <div class="q-pa-md">
-    <q-layout view="lHh Lpr lff" container style="height: 100vh" class="shadow-2 rounded-borders">
-      <q-header elevated style="background-color: #355E3B;">
+    <q-layout
+      view="lHh Lpr lff"
+      container
+      style="height: 100vh"
+      class="shadow-2 rounded-borders"
+    >
+      <q-header elevated style="background-color: #355e3b">
         <q-toolbar>
           <q-toolbar-title>Nutriver</q-toolbar-title>
           <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
         </q-toolbar>
       </q-header>
 
-      <q-drawer
-        v-model="drawer"
-        show-if-above
-        :width="200"
-        :breakpoint="400"
-      >
-      <!-- Manejar las rutas a los distintos componentes -->
-        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+      <q-drawer v-model="drawer" show-if-above :width="200" :breakpoint="400">
+        <!-- Manejar las rutas a los distintos componentes -->
+        <q-scroll-area
+          style="
+            height: calc(100% - 150px);
+            margin-top: 150px;
+            border-right: 1px solid #ddd;
+          "
+        >
           <q-list padding>
             <q-item clickable v-ripple>
               <q-item-section avatar>
-                <q-icon name="inbox" />
+                <q-icon name="person" />
               </q-item-section>
-
               <q-item-section>
-                Registrar
-              </q-item-section>
-            </q-item>
+                <div class="q-pa-sm">
+                  <q-btn-dropdown style="background-color: #fafafa;" label="Usuario">
+                    <q-list>
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Agregar Usuario</q-item-label>
+                        </q-item-section>
+                      </q-item>
 
-            <q-item active clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="star" />
-              </q-item-section>
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Listar Usuario</q-item-label>
+                        </q-item-section>
+                      </q-item>
 
-              <q-item-section>
-                Listar
-              </q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="send" />
-              </q-item-section>
-
-              <q-item-section>
-                Actualizar
-              </q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="eva-" />
-              </q-item-section>
-
-              <q-item-section>
-                Cambiar Contraseña
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Actualizar Usuario</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-btn-dropdown>
+                </div>
               </q-item-section>
             </q-item>
 
             <q-item clickable v-ripple>
               <q-item-section avatar>
-                <q-icon name="eva-" />
+                <q-icon name="business" />
               </q-item-section>
-              <button @click="logout">Cerrar Sesión</button> 
               <q-item-section>
-                
+                <div class="q-pa-sm">
+                  <q-btn-dropdown style="background-color:  #fafafa;" label="Proveedor">
+                    <q-list>
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Agregar Proveedor</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Listar Proveedor</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Actualizar Proveedor</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-btn-dropdown>
+                </div>
               </q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="receipt" />
+              </q-item-section>
+              <q-item-section>
+                <div class="q-pa-sm">
+                  <q-btn-dropdown style="background-color:  #fafafa;" label="Boleta">
+                    <q-list>
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Agregar Boleta</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Listar Boleta</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Actualizar Boleta</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-btn-dropdown>
+                </div>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="power_settings_new" />
+              </q-item-section>
+              <q-btn @click="logout" color="white" text-color="black" label="Cerrar Sesión"></q-btn>
+              <q-item-section> </q-item-section>
             </q-item>
           </q-list>
         </q-scroll-area>
 
-        <q-img class="absolute-top"  style="height: 150px; background-color: #355E3B;">
+        <q-img
+          class="absolute-top"
+          style="height: 150px; background-color: #355e3b"
+        >
           <div class="absolute-bottom bg-transparent">
             <q-avatar size="56px" class="q-mb-sm">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
             <div class="text-weight-bold">Pablo González</div>
             <div>@user</div>
@@ -92,37 +151,40 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
+  setup() {
     return {
       drawer: ref(false),
       userId: null,
-      isAuthenticated: false
-    }
+      isAuthenticated: false,
+      onItemClick () {
+        console.log('Clicked on an Item')
+      }
+    };
   },
   created() {
     this.checkAuth();
   },
   methods: {
     checkAuth() {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (token) {
-        const decoded = JSON.parse(atob(token.split('.')[1]));
+        const decoded = JSON.parse(atob(token.split(".")[1]));
         this.userId = decoded.userId;
         this.isAuthenticated = true;
-        this.$router.push('/dashboard'); // Redirigir al dashboard si está autenticado
+        this.$router.push("/dashboard"); // Redirigir al dashboard si está autenticado
       } else {
-        this.$router.push('/login'); // Redirigir a login si no está autenticado
+        this.$router.push("/login"); // Redirigir a login si no está autenticado
       }
     },
     logout() {
-      localStorage.removeItem('token');
+      localStorage.removeItem("token");
       this.isAuthenticated = false;
       this.userId = null;
-      this.$router.push('/login'); // Redirigir a login después de cerrar sesión
-    }
-  }
-}
+      this.$router.push("/login"); // Redirigir a login después de cerrar sesión
+    },
+  },
+};
 </script>
