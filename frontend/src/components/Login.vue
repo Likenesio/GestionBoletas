84868c33c20f28470457ec05fc9346ecbab9da88
Likenesio <valentinubb@gmail.com>
2024-06-14 19:20:33 -0400

    Se agregan validaciones al login

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,101 +1,94 @@
-<template id="form">
-  <div class="page">
-    <div class="contenedor">
-      <h1>Iniciar Sesión</h1>
-      <form @submit.prevent="login">
-        <input v-model="correo" type="email" placeholder="Correo" required />
-        <input v-model="contrasenia" type="password" placeholder="Contraseña" required />
-        <button type="submit">Iniciar Sesión</button>
-      </form>
-      <div v-if="message">{{ message }}</div>
-    </div>
-  </div>
+<template>
+  <q-layout view="hHh Lpr fFf">
+
+    <!-- Contenido principal -->
+    <q-page-container>
+      <q-page class="login-page">
+        <q-card class="login-card">
+          <q-card-section>
+            <q-card-title class="text-h4">Iniciar Sesión</q-card-title>
+            <q-form @submit="login">
+              <q-input v-model="correo" type="email" label="Correo" dense outlined />
+              <q-input style="margin-top: 5px;" v-model="contrasenia" type="password" label="Contraseña" dense outlined />
+              <q-btn type="submit" color="primary" label="Iniciar Sesión" class="q-mt-md" />
+            </q-form>
+            <q-space />
+            <div v-if="message" class="text-negative">{{ message }}</div>
+          </q-card-section>
+        </q-card>
+      </q-page>
+    </q-page-container>
+
+  </q-layout>
 </template>
-  
-  <script>
-  import axios from '../axios'; // Asegúrate de tener configurado axios
-  
-  export default {
-    data() {
-      return {
-        correo: '',
-        contrasenia: '',
-        message: ''
-      };
-    },
-    methods: {
-      async login() {
-        try {
-          const response = await axios.post('/usuario/login', {
-            correo: this.correo,
-            contrasenia: this.contrasenia
-          });
-          const token = response.data.token;
-          localStorage.setItem('token', token); // Almacenar el token en localStorage
-          this.$router.push('/dashboard'); // Redirigir al dashboard o página principal
-          this.$root.isAuthenticated = true; // Actualizar el estado de autenticación
-          this.$root.userId = JSON.parse(atob(token.split('.')[1])).userId; // Obtener el ID del usuario
-        } catch (error) {
-          this.message = error.response.data.message || 'Error al iniciar sesión';
+
+<script>
+import axios from '../axios';
+
+export default {
+  data() {
+    return {
+      correo: '',
+      contrasenia: '',
+      message: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('/usuario/login', {
+          correo: this.correo,
+          contrasenia: this.contrasenia
+        });
+        const token = response.data.token;
+        localStorage.setItem('token', token);
+        this.$router.push('/dashboard');
+        this.$root.isAuthenticated = true;
+        this.$root.userId = JSON.parse(atob(token.split('.')[1])).userId;
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          this.showMessage('Credenciales incorrectas');
+        } else {
+          this.showMessage('Error al iniciar sesión');
         }
       }
+    },
+    showMessage(message) {
+      this.$q.notify({
+        color: 'negative',
+        position: 'top',
+        message: message,
+        icon: 'report_problem'
+      });
     }
-  };
-  </script>
-  
+  }
+};
+</script>
+
 <style scoped>
-*{
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-.page {
+.login-page {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
 }
 
-.contenedor {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  padding: 5px;
-  }
-  
-  form {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+.login-card {
+  max-width: 400px;
+  width: 100%;
 }
 
-input {
-  padding: 2px 5px;
+.text-h4 {
+  font-size: 1.875rem; /* 30px */
+  line-height: 1.33;
+  margin-bottom: 1rem;
 }
-.message {
+
+.q-mt-md {
+  margin-top: 1.5rem; /* 24px */
+}
+
+.text-negative {
   color: red;
 }
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  background-color: #f8eeee;
-  cursor: pointer;
-  transition: border-color 0.25s;
-}
-button:hover {
-  border-color: #646cff;
-}
-button:focus,
-button:focus-visible {
-  outline: 4px auto -webkit-focus-ring-color;
-}
-h1 {
-  font-size: 4em;
-  line-height: 1.1;
-}
-
 </style>
